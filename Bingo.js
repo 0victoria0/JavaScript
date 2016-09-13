812d54dc377f983780497f4fde10080c8801b6ca
victoria <1491693608@qq.com>
2016-09-13 11:02:32 +0800

    Bingo Card游戏

--- Bingo.js
+++ Bingo.js
@@ -73,6 +73,56 @@ function setSquare(thisSquare){
     }while (usedNums[newNum]);
     usedNums[newNum] = true;
     document.getElementById(currenSquare).innerHTML = newNum;
+    document.getElementById(currenSquare).className = "";
+    document.getElementById(currenSquare).onmousedown = toggleColor;
+}
+
+//点击Square时颜色的变化
+function toggleColor(evt){
+    /*
+        如果evt值存在,就说明用户的浏览器不是IE,可以看到evt的目标.
+        如果是IE浏览器,就要查看window对象的event属性的srcElement.
+    * */
+    if (evt){
+        var thisSquare = evt.target;
+    }else {
+        var thisSquare = window.event.srcElement;
+    }
+    if (thisSquare.className == ""){
+        thisSquare.className = "pickedBG";
+    }else {
+        thisSquare.className = "";
+    }
+    checkWin();
+}
+
+//检查是否获胜
+function checkWin(){
+    var winningOption = -1;//存储用户可能遇到的获胜选项
+    var setSquares = 0;//存储已经单击的格子
+    //数组中的每一个数字都是有效获胜组合的编码值
+    var winners = new Array(31,992,15360,507904,541729,557328,1083458,2162820,4329736,8519745,8659472,16252928);
+
+    for (var i=0; i<24; i++){
+        var currentSquare = "square" + i;
+        if (document.getElementById(currentSquare).className != ""){
+            document.getElementById(currentSquare).className = "pickedBG";
+            setSquares = setSquares | Math.pow(2,i);
+        }
+    }
+    for (var i=0; i<winners.length; i++){
+        if ((winners[i] & setSquares) == winners[i]){
+            winningOption = i;
+        }
+    }
+    if (winningOption > -1){
+        for (var  i=0; i<24; i++){
+            if (winners[winningOption] & Math.pow(2,i)){
+                currentSquare = "square" + i;
+                document.getElementById(currentSquare).className = "winningBG";
+            }
+        }
+    }
 }
 
 //获取0~15的随机整数
